e830fa004d0bc9081187ace70404e0058225d356
Salim Lakhani <salim.lakhani@ucdenver.edu>
2024-02-20 10:07:34 -0700

    Updated java file.

--- app/src/main/java/edu/ucdenver/salimllakhani/githubdemo/MainActivity.java
+++ app/src/main/java/edu/ucdenver/salimllakhani/githubdemo/MainActivity.java
@@ -3,12 +3,21 @@ package edu.ucdenver.salimllakhani.githubdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button okButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        okButton = findViewById(R.id.button);
+
+    }
+
+    public void myMethod1 () {
+
     }
 }
